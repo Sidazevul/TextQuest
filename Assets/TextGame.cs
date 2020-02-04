30d1f99d2761296820113c46b883b1cb906d53be
Sidazevul <60444258+Sidazevul@users.noreply.github.com>
2020-02-04 21:17:06 +0300

    Initial commit

---
+++ Assets/TextGame.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextGame : MonoBehaviour
+{
+    [Header("Elements")]
+    public Text titleText;
+    public Text contentText;
+
+    [Header("Config")]
+    [TextArea]public string TitleText;
+    [Tooltip("123")]public string GameText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        contentText.text = GameText;
+        titleText.text = TitleText;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
